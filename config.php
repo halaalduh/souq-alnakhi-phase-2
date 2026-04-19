e28f42db451a824bce0286187c2c9d8678b1e274
hcchly <haifathefirst@gmail.com>
2026-04-19 20:39:52 +0300

    done home image problem

--- config.php
+++ config.php
@@ -1,10 +1,6 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "root";
-$database = "souq_alnakhil_db";
 
-$conn = new mysqli($host, $user, $password, $database, 8889);
+<?php
+$conn = new mysqli("localhost", "root", "", "souq_alnakhil_db");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
