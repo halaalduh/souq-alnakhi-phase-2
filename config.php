77c5e0d43ee780c737ee73d76cc1154128ba1472
hcchly <haifathefirst@gmail.com>
2026-04-28 20:05:22 +0300

    haifa

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 
 <?php
-$conn = new mysqli("localhost", "root", "", "souq_alnakhil_db");
+$conn = new mysqli("localhost", "root", "", "souq_alnakhil_db",8889);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
